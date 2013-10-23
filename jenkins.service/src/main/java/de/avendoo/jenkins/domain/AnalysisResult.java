97c0ce5bca8f8c52c0a94918b11ab4c9b7bfaeac
atomfrede <atomfrede@gmail.com>
2013-10-23 19:15:08 +0200

    Litte Refactorings.

---
+++ jenkins.service/src/main/java/de/avendoo/jenkins/domain/AnalysisResult.java
@@ -0,0 +1,65 @@
+package de.avendoo.jenkins.domain;
+
+public abstract class AnalysisResult {
+
+	private int numberOfFixedWarnings;
+	private int numberOfHighPriorityWarnings;
+	private int numberOfLowPriorityWarnings;
+	private int numberOfNewWarnings;
+	private int numberOfNormalPriorityWarnings;
+	private int numberOfWarnings;
+	
+	public AnalysisResult setNumberOfFixedWarnings(int numberOfFixedWarnings) {
+		this.numberOfFixedWarnings = numberOfFixedWarnings;
+		return this;
+	}
+
+	public AnalysisResult setNumberOfHighPriorityWarnings(int numberOfHighPriorityWarnings) {
+		this.numberOfHighPriorityWarnings = numberOfHighPriorityWarnings;
+		return this;
+	}
+
+	public AnalysisResult setNumberOfLowPriorityWarnings(int numberOfLowPriorityWarnings) {
+		this.numberOfLowPriorityWarnings = numberOfLowPriorityWarnings;
+		return this;
+	}
+
+	public AnalysisResult setNumberOfNewWarnings(int numberOfNewWarnings) {
+		this.numberOfNewWarnings = numberOfNewWarnings;
+		return this;
+	}
+
+	public AnalysisResult setNumberOfNormalPriorityWarnings(int numberOfNormalPriorityWarnings) {
+		this.numberOfNormalPriorityWarnings = numberOfNormalPriorityWarnings;
+		return this;
+	}
+
+	public AnalysisResult setNumberOfWarnings(int numberOfWarnings) {
+		this.numberOfWarnings = numberOfWarnings;
+		return this;
+	}
+	
+	public int getNumberOfFixedWarnings() {
+		return numberOfFixedWarnings;
+	}
+	
+	public int getNumberOfHighPriorityWarnings() {
+		return numberOfHighPriorityWarnings;
+	}
+	
+	public int getNumberOfLowPriorityWarnings() {
+		return numberOfLowPriorityWarnings;
+	}
+	
+	public int getNumberOfNewWarnings() {
+		return numberOfNewWarnings;
+	}
+	
+	public int getNumberOfNormalPriorityWarnings() {
+		return numberOfNormalPriorityWarnings;
+	}
+	
+	public int getNumberOfWarnings() {
+		return numberOfWarnings;
+	}
+}
--- jenkins.service/src/main/java/de/avendoo/jenkins/domain/Findbugs.java
+++ jenkins.service/src/main/java/de/avendoo/jenkins/domain/Findbugs.java
@@ -2,69 +2,7 @@ package de.avendoo.jenkins.domain;
 
 import java.io.Serializable;
 
-public class Findbugs implements Serializable {
+public class Findbugs extends AnalysisResult implements Serializable {
 
-	private static final long serialVersionUID = -7494649818619999997L;
-	
-	private int numberOfFixedWarnings;
-	private int numberOfHighPriorityWarnings;
-	private int numberOfLowPriorityWarnings;
-	private int numberOfNewWarnings;
-	private int numberOfNormalPriorityWarnings;
-	private int numberOfWarnings;
-	
-	
-	public Findbugs setNumberOfFixedWarnings(int numberOfFixedWarnings) {
-		this.numberOfFixedWarnings = numberOfFixedWarnings;
-		return this;
-	}
-	
-	public Findbugs setNumberOfHighPriorityWarnings(int numberOfHighPriorityWarnings) {
-		this.numberOfHighPriorityWarnings = numberOfHighPriorityWarnings;
-		return this;
-	}
-	
-	public Findbugs setNumberOfLowPriorityWarnings(int numberOfLowPriorityWarnings) {
-		this.numberOfLowPriorityWarnings = numberOfLowPriorityWarnings;
-		return this;
-	}
-	
-	public Findbugs setNumberOfNewWarnings(int numberOfNewWarnings) {
-		this.numberOfNewWarnings = numberOfNewWarnings;
-		return this;
-	}
-	
-	public Findbugs setNumberOfNormalPriorityWarnings(int numberOfNormalPriorityWarnings) {
-		this.numberOfNormalPriorityWarnings = numberOfNormalPriorityWarnings;
-		return this;
-	}
-	
-	public Findbugs setNumberOfWarnings(int numberOfWarnings) {
-		this.numberOfWarnings = numberOfWarnings;
-		return this;
-	}
-	
-	public int getNumberOfFixedWarnings() {
-		return numberOfFixedWarnings;
-	}
-	
-	public int getNumberOfHighPriorityWarnings() {
-		return numberOfHighPriorityWarnings;
-	}
-	
-	public int getNumberOfLowPriorityWarnings() {
-		return numberOfLowPriorityWarnings;
-	}
-	
-	public int getNumberOfNewWarnings() {
-		return numberOfNewWarnings;
-	}
-	
-	public int getNumberOfNormalPriorityWarnings() {
-		return numberOfNormalPriorityWarnings;
-	}
-	
-	public int getNumberOfWarnings() {
-		return numberOfWarnings;
-	}
+	private static final long serialVersionUID = -7494649818619999997L;	
 }
