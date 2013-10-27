15e7129a87b45fe1c107c58204b955060e6ca5d5
Frederik Hahne <atomfrede@gmail.com>
2013-10-27 16:07:35 +0100

    Updated Package Structure.

--- jenkins.web/src/main/java/de/avendoo/jenkins/controller/web/HelloController.java
+++ jenkins.web/src/main/java/de/avendoo/jenkins/controller/web/HelloController.java
@@ -5,8 +5,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import de.avendoo.jenkins.rest.BuildJobService;
+import de.avendoo.jenkins.rest.CheckstyleService;
+import de.avendoo.jenkins.rest.DuplicatedCodeService;
+import de.avendoo.jenkins.rest.FindbugsService;
+import de.avendoo.jenkins.rest.TestReportService;
+
 @Controller
 public class HelloController {
+	
+	BuildJobService buildJobService;
+	TestReportService restReportService;
+	FindbugsService findbugsService;
+	CheckstyleService checkstykeService;
+	DuplicatedCodeService dryService;
 	
 	@RequestMapping(value="/hello", method = RequestMethod.GET)
 	public ModelAndView helloWorld() {
