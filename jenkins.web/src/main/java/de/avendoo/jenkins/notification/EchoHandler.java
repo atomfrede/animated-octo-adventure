f1a33e8a5b69bc7101f083a3195e54aa252ab026
atomfrede <atomfrede@gmail.com>
2014-01-22 20:24:40 +0100

    Simple Websocket integration

---
+++ jenkins.web/src/main/java/de/avendoo/jenkins/notification/EchoHandler.java
@@ -0,0 +1,55 @@
+package de.avendoo.jenkins.notification;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class EchoHandler extends TextWebSocketHandler {
+
+	private final Map<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
+
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session)
+			throws Exception {
+		sessions.put(session.getId(), session);
+	}
+
+	@Override
+	public void afterConnectionClosed(WebSocketSession session,
+			CloseStatus status) throws Exception {
+		sessions.remove(session.getId());
+	}
+
+	@Override
+	public void handleTextMessage(WebSocketSession session, TextMessage message)
+			throws Exception {
+		session.sendMessage(message);
+	}
+
+	@Async
+	public void sendToAll(String message) {
+		System.out.println("Send to all");
+		TextMessage textMessage = new TextMessage(message);
+
+		for (WebSocketSession session : sessions.values()) {
+			if (session.isOpen()) {
+				try {
+					session.sendMessage(textMessage);
+				} catch (IOException e) {
+					sessions.remove(session.getId());
+					e.printStackTrace();
+				}
+			} else {
+				sessions.remove(session.getId());
+			}
+		}
+	}
+}
---
+++ jenkins.web/src/main/java/de/avendoo/jenkins/notification/NotificationHelper.java
@@ -0,0 +1,26 @@
+package de.avendoo.jenkins.notification;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificationHelper {
+
+	boolean started = false;
+	
+	@Autowired
+	EchoHandler websocketHandler;
+	
+	@Async
+	public void startNotificationHelper() throws InterruptedException{
+		if(!started) {
+			started = true;
+			while(started) {
+				websocketHandler.sendToAll("Notification");
+				Thread.sleep(10000);
+			}
+		}
+		
+	}
+}
